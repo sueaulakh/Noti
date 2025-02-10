3c3a3e66704593495968767386edec2c35909c2f
Sukhali Singh Aulakh <156894204+sueaulakh@users.noreply.github.com>
2025-02-10 19:15:49 +0530

    Add files via upload

---
+++ notidApp.swift
@@ -0,0 +1,17 @@
+//
+//  notidApp.swift
+//  notid
+//
+//  Created by HAPPY on 09/02/25.
+//
+
+import SwiftUI
+
+@main
+struct notidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
